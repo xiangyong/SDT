1ea305bd31b9851ec9aad350c125b4cae23c78b0
ronghai.ma <github_ai@126.com>
2014-08-19 12:56:36 +0800

    finish add project to parent pom
    finish add project to parent pom

--- src/sdt/wizards/newproject/NewProjectWizard.java
+++ src/sdt/wizards/newproject/NewProjectWizard.java
@@ -1,10 +1,10 @@
 package sdt.wizards.newproject;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.Charset;
 
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
@@ -22,6 +22,8 @@ import org.eclipse.ui.PlatformUI;
 import sdt.SDTPlugin;
 import sdt.wizards.NewPreviewWizardPage;
 import sdt.wizards.NewWizard;
+
+import com.google.common.io.Files;
 
 public class NewProjectWizard extends NewWizard {
 
@@ -53,7 +55,7 @@ public class NewProjectWizard extends NewWizard {
 
 		JavaCore.create(project);
 
-		addToMainPom(project);
+		addToMainPom();
 
 		// # add to ace pom
 		// find proj
@@ -78,84 +80,92 @@ public class NewProjectWizard extends NewWizard {
 		return f;
 	}
 
-	private void addToMainPom(IProject project) {
+	private void addToMainPom() {
 		NewProjectState data = (NewProjectState) this.previewPage.data;
-		// # add to main pom
-		// find proj
-		// -z 
-		//   find tpl
-		//   -n
-		//     find @tpl "</dependency>"
-		//     insert @>"</dependency>" proj
-		// find proj_module
-		// -z
-		//   find tpl module
-		//   -n
-		//     insert @tpl_module prject_module
+		File pom = getMainPom(data.name);
 
+		// read
+		String txt = null;
 		try {
-			URI uri = project.getRawLocationURI();
-			File file = new File(uri);
-			if (!file.exists())
-				return;
-			File parent = file;
-			for (int i = 0; i < 3; i++) {
-				if (parent.getParentFile().exists()) {
-					parent = parent.getParentFile();
-				}
-			}
-			System.err.println(parent);
-
-			File[] files = parent.listFiles(new FilenameFilter() {
-				@Override
-				public boolean accept(File f, String name) {
-					return name.equals("pom.xml");
-				}
-			});
-			if (files.length == 0)
-				return;
-
-			File pom = files[0];
-			if (!pom.exists())
-				return;
-
-			FileReader f = new FileReader(pom);
-			int l = (int) pom.length();
-			char[] cs = new char[l];
-			f.read(cs);
-			f.close();
-			String txt = String.valueOf(cs);
-
-			if (txt.contains(data.name))
-				return;
-
-			String tpl = data.system + "-" + data.type;
-			if (!txt.contains(tpl))
-				return;
-
-			StringBuffer buff = new StringBuffer(txt);
-			// dependency
-			int p = buff.indexOf(tpl);
-			String key = "</dependency>";
-			int d = buff.indexOf(key, p) + key.length();
-
-			Velocity.init();
-			VelocityContext context = new VelocityContext();
-			context.put("system", data.system);
-			context.put("project", data.name);
-			context.put("type", "parent");
-			String content = "\n" + SDTPlugin.getTpl(context, "tpl/proj/dependency.vm");
-
-			buff.insert(d, content);
-
-			// module
-			FileWriter w = new FileWriter(pom);
-			w.write(buff.toString());
-			w.flush();
-			w.close();
-		} catch (Exception e) {
+			txt = Files.toString(pom, Charset.forName("GBK"));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		if (txt == null || txt.contains(data.name))
+			return;
+
+		StringBuffer buff = new StringBuffer(txt);
+
+		// init context
+		Velocity.init();
+		VelocityContext context = new VelocityContext();
+		context.put("system", data.system);
+		context.put("project", data.name);
+
+		// dependency
+		{
+			context.put("type", "parent");
+			String content = "\n" + SDTPlugin.getTpl(context, "tpl/proj/dependency.vm");
+			String key1 = data.name;
+			String key2 = data.system + "-" + data.type;
+			String key3 = "</dependency>";
+			insertString(buff, content, key1, key2, key3);
+		}
+		// module
+		{
+			context.put("type", "module");
+			String content = "\n" + SDTPlugin.getTpl(context, "tpl/proj/dependency.vm");
+			String key1 = "app/" + data.name.substring(data.name.indexOf("-") + 1).replaceFirst("-", "/");
+			String key2 = "app/" + data.type.replaceFirst("-", "/");
+			String key3 = "</module>";
+			insertString(buff, content, key1, key2, key3);
+		}
+
+		// write
+		try {
+			Files.write(buff.toString().getBytes(), pom);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void insertString(StringBuffer buff, String value, String key1, String key2, String key3) {
+		if (buff.indexOf(key1) > 0 || buff.indexOf(key2) < 0)
+			return;
+
+		int i = buff.indexOf(key2);
+		int j = buff.indexOf(key3, i) + key3.length();
+		buff.insert(j, value);
+	}
+
+	private File getMainPom(String name) {
+		IProject project = SDTPlugin.getProject(name);
+		URI uri = project.getRawLocationURI();
+		File file = new File(uri);
+		if (!file.exists())
+			return null;
+
+		int l = name.endsWith("-assembly-templage") ? 2 : 3;
+		File parent = file;
+		for (int i = 0; i < l; i++) {
+			if (parent.getParentFile().exists()) {
+				parent = parent.getParentFile();
+			} else {
+				return null;
+			}
+		}
+
+		File[] files = parent.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File f, String name) {
+				return name.equals("pom.xml");
+			}
+		});
+		if (files.length == 0)
+			return null;
+
+		return files[0];
 	}
 
 	@Override
