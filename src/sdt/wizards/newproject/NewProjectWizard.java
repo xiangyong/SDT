6ccf1acb18d5b61c9b602dc1724b8bd7f66cace3
ronghai.ma <github_ai@126.com>
2014-08-19 10:00:38 +0800

    add dependency to main pom
    add dependency to main pom

--- src/sdt/wizards/newproject/NewProjectWizard.java
+++ src/sdt/wizards/newproject/NewProjectWizard.java
@@ -1,5 +1,13 @@
 package sdt.wizards.newproject;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.net.URI;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -38,15 +46,116 @@ public class NewProjectWizard extends NewWizard {
 			ws = wsm.createWorkingSet(data.system, new IAdaptable[0]);
 			wsm.addWorkingSet(ws);
 		}
-		
+
 		wsm.addToWorkingSets(project, new IWorkingSet[] { ws });
 
-		// super performFinish
 		boolean f = super.performFinish(monitor);
 
 		JavaCore.create(project);
 
+		addToMainPom(project);
+
+		// # add to ace pom
+		// find proj
+		// -z 
+		//   find tpl
+		//   -n
+		//     find @tpl "</artifactItem>"
+		//     insert @>"</artifactItem>" proj
+
+		// # add to test pom
+		// find proj
+		// -z 
+		//   find "</dependencies>"
+		//   insert @>"</dependencies>" proj
+
+		// # add to test project
+		// .project
+		// .classpath
+
+		// super performFinish
+
 		return f;
+	}
+
+	private void addToMainPom(IProject project) {
+		NewProjectState data = (NewProjectState) this.previewPage.data;
+		// # add to main pom
+		// find proj
+		// -z 
+		//   find tpl
+		//   -n
+		//     find @tpl "</dependency>"
+		//     insert @>"</dependency>" proj
+		// find proj_module
+		// -z
+		//   find tpl module
+		//   -n
+		//     insert @tpl_module prject_module
+
+		try {
+			URI uri = project.getRawLocationURI();
+			File file = new File(uri);
+			if (!file.exists())
+				return;
+			File parent = file;
+			for (int i = 0; i < 3; i++) {
+				if (parent.getParentFile().exists()) {
+					parent = parent.getParentFile();
+				}
+			}
+			System.err.println(parent);
+
+			File[] files = parent.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File f, String name) {
+					return name.equals("pom.xml");
+				}
+			});
+			if (files.length == 0)
+				return;
+
+			File pom = files[0];
+			if (!pom.exists())
+				return;
+
+			FileReader f = new FileReader(pom);
+			int l = (int) pom.length();
+			char[] cs = new char[l];
+			f.read(cs);
+			f.close();
+			String txt = String.valueOf(cs);
+
+			if (txt.contains(data.name))
+				return;
+
+			String tpl = data.system + "-" + data.type;
+			if (!txt.contains(tpl))
+				return;
+
+			StringBuffer buff = new StringBuffer(txt);
+			// dependency
+			int p = buff.indexOf(tpl);
+			String key = "</dependency>";
+			int d = buff.indexOf(key, p) + key.length();
+
+			Velocity.init();
+			VelocityContext context = new VelocityContext();
+			context.put("system", data.system);
+			context.put("project", data.name);
+			context.put("type", "parent");
+			String content = "\n" + SDTPlugin.getTpl(context, "tpl/proj/dependency.vm");
+
+			buff.insert(d, content);
+
+			// module
+			FileWriter w = new FileWriter(pom);
+			w.write(buff.toString());
+			w.flush();
+			w.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
