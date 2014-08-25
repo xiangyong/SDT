235f519e9154d23af761ace68170110b894a8f31
ronghai.ma <github_ai@126.com>
2014-08-25 22:23:20 +0800

    refactor
    refactor

--- src/sdt/core/_.java
+++ src/sdt/core/_.java
@@ -1,5 +1,6 @@
 package sdt.core;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.JarURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 import javax.script.ScriptContext;
@@ -19,6 +21,7 @@ import javax.script.SimpleBindings;
 
 public class _ {
 
+	// TODO Template Engine
 	public static String f(String txt, Map<String, ?> ctx) {
 		String[] ss = txt.split("\n");
 		StringBuffer js = new StringBuffer("_='';");
@@ -92,6 +95,9 @@ public class _ {
 		return f.toString();
 	}
 
+	// TODO Template Engine ~~
+
+	// TODO Read From File
 	public static String readFromFile(File file) {
 		StringBuffer f = new StringBuffer();
 		BufferedReader br = null;
@@ -117,6 +123,7 @@ public class _ {
 		return f.toString();
 	}
 
+	// TODO Write To File
 	public static void writeToFile(File file, String content) {
 		FileWriter fw = null;
 		try {
@@ -138,6 +145,7 @@ public class _ {
 		}
 	}
 
+	// TODO Read From Jar
 	public static String readFromJar(String file) { // ***.jar!/***.java
 		System.err.println(file);
 		StringBuffer f = new StringBuffer();
@@ -155,4 +163,68 @@ public class _ {
 		}
 		return f.toString();
 	}
+
+	// Translate English To Chinese
+	public static String translate(String word) {
+		String f = null;
+		InputStream in = null;
+		BufferedInputStream bis = null;
+		try {
+			URL fUrl = new URL("http://fanyi.baidu.com/v2transapi?from=en&to=cn&query=" + word);
+			URLConnection connection = fUrl.openConnection();
+			in = connection.getInputStream();
+			int len = 256;
+			bis = new BufferedInputStream(in, len);
+			byte[] bs = new byte[len];
+			bis.read(bs);
+			String s = new String(bs);
+			int p = s.indexOf('"', 90);
+			f = decode(s.substring(90, p));
+			bis.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (in != null) {
+					in.close();
+					in = null;
+				}
+				if (bis != null) {
+					bis.close();
+					bis = null;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return f;
+	}
+
+	public static String decode(String unicodeStr) {
+		if (unicodeStr == null) {
+			return null;
+		}
+		StringBuffer retBuf = new StringBuffer();
+		int maxLoop = unicodeStr.length();
+		for (int i = 0; i < maxLoop; i++) {
+			if (unicodeStr.charAt(i) == '\\') {
+				if ((i < maxLoop - 5) && ((unicodeStr.charAt(i + 1) == 'u') || (unicodeStr.charAt(i + 1) == 'U')))
+					try {
+						retBuf.append((char) Integer.parseInt(unicodeStr.substring(i + 2, i + 6), 16));
+						i += 5;
+					} catch (NumberFormatException localNumberFormatException) {
+						retBuf.append(unicodeStr.charAt(i));
+					}
+				else
+					retBuf.append(unicodeStr.charAt(i));
+			} else {
+				retBuf.append(unicodeStr.charAt(i));
+			}
+		}
+		return retBuf.toString();
+	}
+
 }
